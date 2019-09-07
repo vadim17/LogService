12052bfcf18380cbb8d07508899b69f686f82443
adamo <adamosoftware@gmail.com>
2019-09-07 13:48:07 -0400

    code cleanup and some comments

--- SampleApp/Pages/Index.cshtml.cs
+++ SampleApp/Pages/Index.cshtml.cs
@@ -9,9 +9,12 @@ namespace SampleApp.Pages
 {
     public class IndexModel : PageModel
     {
+        // see https://code-maze.com/global-error-handling-aspnetcore/#custommiddleware
+        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/write?view=aspnetcore-2.2
+
         public void OnGet()
         {
 
-        }
+        }       
     }
 }
