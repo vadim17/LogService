bb726c37dcccb954e9c4971330ccf665ecb58755
adamo <adamosoftware@gmail.com>
2019-09-08 12:22:11 -0400

    renamed LogException -> InternalLogException, and removed Loggingbase due to filename case issue

---
+++ LogService/Exceptions/InternalLogException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogService.Client.Exceptions
+{
+    public class InternalLogException : Exception
+    {
+        /// <summary>
+        /// Thrown when the Azure table storage logging fails
+        /// </summary>        
+        public InternalLogException(Exception innerException) : base($"Internal logging exception: {innerException.Message}", innerException)
+        {
+        }
+    }
+}
